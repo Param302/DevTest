05e57bd6b34833cbd8ec30e939bc286a425c7da6
Parampreet Singh <rubercuber.30@gmail.com>
2024-10-25 23:42:56 +0530

    Displayed uploaded file content

--- DevTest/file_summary/views.py
+++ DevTest/file_summary/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +7,12 @@ def upload_file(request):
     if request.method == "GET":
         return render(request, 'upload_file.html')
     
+    if not request.FILES:
+        return render(request, 'upload_file.html', {"error": "Please upload excel file"})
+    
+    file = request.FILES['file']
+    if not file.name.endswith('.xlsx'):
+        return render(request, 'upload_file.html', {"error": "Please upload excel file"})
+    
+    data = pd.read_excel(file)
+    return render(request, 'report.html', {"data": data.head()})
